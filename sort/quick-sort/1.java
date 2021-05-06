535473a62efdb062dce4ec2ecf3c0970f2ff3615
xipper <864353360@qq.com>
2021-05-06 20:40:23 +0800

    原数组快排

--- sort/quick-sort/1.java
+++ sort/quick-sort/1.java
@@ -1,15 +1,24 @@
 class Solution {
-    public void quicksort(int [] nums){
-        int j = 0;
-        for(int i = 0;i<nums.length;++i){
-            if(nums[i] != 0){
-                nums[j] = nums[i];
-                if(j!= i){
-                    nums[i] = 0;
-                }
-                j++;
-            }
-        }
+    public void quicksort(int[] array, int begin, int end) {
+        if (end <= begin) return;
+        int pivot = partition(array, begin, end);
+        quicksort(array, begin, pivot - 1);
+        quicksort(array, pivot + 1, end);
     }
 
+    static int partition(int[] a, int begin, int end) {
+        int pivot = end, counter = begin;
+        for (int i = begin; i < end; i++) {
+            if (a[i] < a[pivot]) {
+                int temp = a[counter];
+                a[counter] = a[i];
+                a[i] = temp;
+                counter++;
+            }
+        }
+        int temp = a[pivot];
+        a[pivot] = a[counter];
+        a[counter] = temp;
+        return counter;
+    }
 }
--- sort/quick-sort/1.js
+++ sort/quick-sort/1.js
@@ -1,12 +1,31 @@
-function quicksort(nums) {
-    let j = 0
-    nums.forEach((num, idx)=>{
-        if(num!==0){
-            nums[j] = num
-            if(j!==idx){
-                nums[idx] = 0
-            }
-            j++
-        }
-    })
+function quicksort(array,begin,end) {
+    if(typeof begin ==='undefined') begin = 0
+    if(typeof end ==='undefined') end = array.length-1
+    if (end <= begin) return;
+    let pivot = partition(array, begin, end);
+    quicksort(array, begin, pivot - 1);
+    quicksort(array, pivot + 1, end);
 }
+
+function partition(a,begin,end) {
+    let pivot = end,count = begin
+    for(let i =begin;i<end;i++){
+        if(a[i]<a[pivot]){
+            let temp = a[count]
+            a[count] = a[i]
+            a[i] = temp
+            count++
+        }
+    }
+    let temp = a[count]
+    a[count] = a[pivot]
+    a[pivot] = temp
+    return count
+}
+
+const arr = [2,1,4,5,6,3,4,56,1,3,54]
+
+quicksort(arr)
+
+console.log(arr)
+
