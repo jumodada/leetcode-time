7fdddfad50ab62d811dd425a8d8e2dea01c16846
xipper <864353360@qq.com>
2021-05-08 11:16:58 +0800

    汉诺塔

---
+++ linked list/Hanota/递归.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void hanota(List<Integer> A, List<Integer> B, List<Integer> C) {
+        moveTower(A.size(),A,B,C);
+    }
+    public void AToC(List<Integer> A,List<Integer> C) {
+        C.add(A.remove(A.size()-1));
+    }
+
+    public void moveTower(int n, List<Integer> A, List<Integer> B, List<Integer> C) {
+        if(n==1){
+            AToC(A,C);
+        }else{
+            moveTower(n-1,A,C,B);
+            AToC(A,C);
+            moveTower(n-1,B,A,C);
+        }
+    }
+}
